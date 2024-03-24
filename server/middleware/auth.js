ca56526869a4f2adf64849e3a68a47631255305d
jaswalsahil786@gmail.com <Sahil.Jaswal@lntinfotech.com>
2024-03-24 13:03:44 +0530

    adding dotenv file for secrets

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,11 +1,10 @@
 const jwt = require('jsonwebtoken');
-const SECRET = 'SECr3t';  // This should be in an environment variable in a real application
 
 const authenticateJwt = (req, res, next) => {
   const authHeader = req.headers.authorization;
   if (authHeader) {
     const token = authHeader.split(' ')[1];
-    jwt.verify(token, SECRET, (err, user) => {
+    jwt.verify(token, process.env.SECRET, (err, user) => {
       if (err) {
         return res.sendStatus(403);
       }
